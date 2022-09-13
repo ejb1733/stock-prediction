7e9dbaeb3c8ff44b9ee46fc5ab7ad786073aeab4
ejb <ethanjbillings@gmail.com>
2022-09-13 14:08:26 -0400

    add data for AAPL ticker

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tracemalloc import start
 import numpy as np
 import matplotlib.pyplot as pyplot
 import pandas as pd
@@ -9,3 +10,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Dropout, LSTM
+
+AAPL_TICKER = 'AAPL'
+TSLA_TICKER = 'TSLA'
+
+company = AAPL_TICKER
+start = date.datetime(2002, 7, 7)
+today = date.datetime.today()
+data = web.DataReader(company, 'yahoo', start, today)
